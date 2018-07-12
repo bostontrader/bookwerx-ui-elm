e57196a53ad323a2e44909776b97266ad79cc7e7
Thomas Radloff <bostontrader@gmail.com>
2018-07-12 16:54:54 +0800

    Tweak testing.

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
-  NO_BWCORE_URL_DEFINED: 'No bookwerx-core url defined',
+  NO_BWCORE_URL_DEFINED: 'No bookwerx-core url defined (BWCORE_URL)',
   NO_BWUI_URL_DEFINED: 'No bookwerx-ui url is defined',
   // NO_CONNECTION_TO_MONGODB_DEFINED: 'No connection to mongodb defined',
-  NO_OPERATING_MODE_DEFINED: 'No operating mode defined',
-  NO_PORT_DEFINED: 'No listening port defined'
+  NO_OPERATING_MODE_DEFINED: 'No operating mode defined (BWUI_MODE)',
+  NO_PORT_DEFINED: 'No listening port defined (BWUI_PORT)'
 }
