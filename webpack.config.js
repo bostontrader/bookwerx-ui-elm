4ed8629267e30a10bea8c08c948958e0cce954c9
Thomas Radloff <bostontrader@gmail.com>
2018-07-16 12:25:52 +0800

    Tweak Travis CI.

--- webpack.config.js
+++ webpack.config.js
@@ -11,36 +11,36 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.(css|scss|sass)$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'file-loader?name=[name].[ext]'
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader: 'elm-webpack-loader?verbose=true&warn=true',
-        options: {
-          cwd: path.join(__dirname, '/elm')
-        }
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-        use: [{
-          loader: 'url-loader',
-           options: {
-            limit: 10000, // Convert images < 8kb to base64 strings
-            name: '/[name].[ext]'
-           }
-        }]
-      }
+      //{
+      //  test: /\.(css|scss|sass)$/,
+      //  use: [
+      //    'style-loader',
+      //    'css-loader'
+      //  ]
+      //},
+      // {
+      //  test: /\.html$/,
+      //  exclude: /node_modules/,
+      //  loader: 'file-loader?name=[name].[ext]'
+      //},
+      //{
+      //  test: /\.elm$/,
+      //  exclude: [/elm-stuff/, /node_modules/],
+      //  loader: 'elm-webpack-loader?verbose=true&warn=true',
+      //  options: {
+      //    cwd: path.join(__dirname, '/elm')
+      // }
+      //},
+      //{
+      //  test: /\.(png|jp(e*)g|svg)$/,
+      //  use: [{
+      //    loader: 'url-loader',
+      //     options: {
+      //      limit: 10000, // Convert images < 8kb to base64 strings
+      //      name: '/[name].[ext]'
+      //     }
+      //  }]
+      //}
     ]
   }
 }
