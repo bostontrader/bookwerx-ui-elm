00035d5763b17e3858da062ad4899f961ef2305a
Thomas Radloff <bostontrader@gmail.com>
2018-07-16 12:44:42 +0800

    Tweak Travis CI.

--- webpack.config.js
+++ webpack.config.js
@@ -23,14 +23,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'file-loader?name=[name].[ext]'
       },
-      //{
-      //  test: /\.elm$/,
-      //  exclude: [/elm-stuff/, /node_modules/],
-      //  loader: 'elm-webpack-loader?verbose=true&warn=true',
-      //  options: {
-      //    cwd: path.join(__dirname, '/elm')
-      // }
-      //},
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader: 'elm-webpack-loader?verbose=true&warn=true',
+        options: {
+          cwd: path.join(__dirname, '/elm')
+       }
+      },
       {
         test: /\.(png|jp(e*)g|svg)$/,
         use: [{
